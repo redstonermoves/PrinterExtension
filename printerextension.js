d3e6d10525128ba77de7e32033dc63af9f40c29f
Mrcomputer1 <Mrcomputer1@users.noreply.github.com>
2015-07-15 15:30:13 +1000

    Update printerextension.js

--- printerextension.js
+++ printerextension.js
@@ -1,32 +1,65 @@
 (function(ext){
 	
+	$.ajax({
+
+        async:false,
+
+        type:'GET',
+
+        url:'https://www.google.com/cloudprint/client/cpgadget.js',
+		
+        data:null,
+        
+        success: function(){},
+		
+		error: function(){},
+
+        dataType:'script'
+
+    });
+	ext.version = "v1.0";
+	
 	ext._shutdown = function() {};
 	
 	ext._getStatus = function() {
-		return {status: 2, msg: '((READY)) Printer Extension v1.0 [MRCOMPUTER1]'};
+		return {status: 2, msg: '((READY)) Printer Extension ' + ext.version + ' [MRCOMPUTER1]'};
 	};
 	
 	ext.printText = function(text){
-		var printWindow = window.open("", "Printer Extension v1.0");
-		printWindow.document.title = "Printer Extension v1.0";
-		printWindow.document.write("<head><title>Printer Extension v1.0</title></head>");
-		printWindow.document.write("<small>Printed with ScratchX with extension Printer Extension v1.0 by Mrcomputer1</small><hr color='black'>" + text + "</body>");
+		var printWindow = window.open("", "Printer Extension " + ext.version);
+		printWindow.document.title = "Printer Extension " + ext.version;
+		printWindow.document.write("<head><title>Printer Extension " + ext.version + "</title></head>");
+		printWindow.document.write(text + "<br><hr color='black'><small>Printed using Printer Extension " + ext.version + " by Mrcomputer1</small>" + "</body>");
 		printWindow.print();
 	};
 	
 	ext.printImage = function(imageURL){
-		var printWindow = window.open("", "Printer Extension v1.0");
-		printWindow.document.title = "Printer Extension v1.0";
-		printWindow.document.write("<head><title>Printer Extension v1.0</title></head>");
+		var printWindow = window.open("", "Printer Extension " + ext.version);
+		printWindow.document.title = "Printer Extension " + ext.version;
+		printWindow.document.write("<head><title>Printer Extension " + ext.version + "</title></head>");
 		var text = "<img src='" + imageURL + "'>";
-		printWindow.document.write("<small>Printed with ScratchX with extension Printer Extension v1.0 by Mrcomputer1</small><hr color='black'>" + text + "</body>");
+		printWindow.document.write(text + "<br><hr color='black'><small>Printed using Printer Extension " + ext.version + " by Mrcomputer1</small>" + "</body>");
 		printWindow.print();
-	}
+	};
+	
+	ext.GCPtext = function(text){
+		var gcpGadget = new cloudprint.Gadget();
+		gcpGadget.setPrintDocument("text/html", "Printer Extension " + ext.version, text + "<br><hr color='black'><small>Printed using Printer Extension " + ext.version + " by Mrcomputer1</small>");
+		gcpGadget.openPrintDialog();
+	};
+	
+	ext.GCPimage = function(imageURL){
+		var gcpGadget = new cloudprint.Gadget();
+		gcpGadget.setPrintDocument("text/html", "Printer Extension " + ext.version, "<img src='" + imageURL + "'>" + "<br><hr color='black'>" + "<small>Printed using Printer Extension " + ext.version + " by Mrcomputer1</small>");
+		gcpGadget.openPrintDialog();
+	};
 	
 	var blocksMenusURL = {
 		blocks: [
 			[' ', 'Print HTML %s', 'printText', ''],
 			[' ', 'Print Image %s', 'printImage', ''],
+			[' ', 'Google Cloud Print HTML %s', 'GCPtext', ''],
+			[' ', 'Google Cloud Print Image %s', 'GCPimage', ''],
 		],
 		url: 'http://mrcomputer1.github.io/PrinterExtension/'
 	};
